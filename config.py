d29751a3dc3f3b08e5714274ada5e82bd1487694
marina-abramova <138307966+marina-abramova@users.noreply.github.com>
2023-07-01 17:16:24 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+community_token = 'vk1.a.WZM7HE0rHEHoAIF9IJ18EeJSG96gg3Emo7LPGuUK0opF-Zleu4kjPADiYrw'
+access_token = 'vk1.a.ELQXBVDkkV-WdvTQYQjUtTNn0TU_MXNpjGQTb8mPfLgJd8STXVJiZTOJ0rMPh1'
+
+DSN = "postgresql://postgres:178757707@localhost:5432/VKinder"
+
+developer_id = 1
+
+count = 50 # количество запрашиваемых анкет за один запрос
+
